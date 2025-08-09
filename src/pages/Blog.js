6a5495a8c99f692b797a904dd5fc0e98a52618a3
Nade04 <86410129+Nade0478@users.noreply.github.com>
2025-08-09 03:58:25 +0200

    mise en place du front

---
+++ src/pages/Blog.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+const Blog = () => {
+  return (
+    <div>
+        <Navbar />
+      <h1>Bienvenu sur mon blog</h1>
+    </div>
+  );
+};
+export default Blog;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+const Home = () => {
+return (
+<div>
+<Navbar />
+<h1>Bienvenu sur la page d'accueil</h1>
+</div>
+);
+};
+export default Home;
